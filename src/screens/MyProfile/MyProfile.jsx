f8b14bf8b37d5bb4c8e5afb9211cb8635157b788
German Rodriguez <germanson@gmail.com>
2019-08-26 03:08:11 -0700

    My profile page shows logged user info

--- src/screens/MyProfile/MyProfile.jsx
+++ src/screens/MyProfile/MyProfile.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const MyProfile = () => (
-  <h1>My Profile</h1>
+import './my-profile.scss';
+
+const MyProfile = ({ user }) => (
+  <div id="my-profile">
+    <h1>My Profile</h1>
+    <img src={user.image} className="picture" />
+    <div className="info">
+      <ul>
+        <li><strong>Display name:</strong><span>{user.name}</span></li>
+        <li><strong>Id:</strong><span>{user.spotifyId}</span></li>
+        <li><strong>Email:</strong><span>{user.email}</span></li>
+        <li><strong>Spotify URI:</strong><span><a href={user.spotifyUrl}>{user.spotifyUrl}</a></span></li>
+      </ul>
+    </div>
+  </div>
 );
 
-export default connect(null, {})(MyProfile);
+const mapStateToProps = ({ session }) => ({
+  user: session.user,
+});
+
+export default connect(mapStateToProps, {})(MyProfile);
